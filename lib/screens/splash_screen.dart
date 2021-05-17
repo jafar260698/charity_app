397e8122d7f606af4764249f439ea11fdd3b4784
jafar260698 <temirov_temirov@mail.ru>
2021-05-18 00:05:59 +0500

    add screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,45 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CupertinoActivityIndicator(),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                "getTranslated(context, 'copyright')",
+                textScaleFactor: 1.0,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 13, fontFamily: 'Montserrat'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+}
