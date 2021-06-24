e126228f6ee2bd60640854f7f013d17f7a47d9f7
j.temirov@yt.uz <qazwsxas>
2021-06-24 08:15:45 +0500

    -update

--- lib/view/screens/home/home_viewmodel.dart
+++ lib/view/screens/home/home_viewmodel.dart
@@ -1,5 +1,6 @@
 
 import 'package:charity_app/model/category.dart';
+import 'package:charity_app/model/forum/forum_category.dart';
 import 'package:charity_app/persistance/api_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:stacked/stacked.dart';
@@ -10,16 +11,31 @@ class HomeViewModel extends BaseViewModel{
   TextEditingController _search = new TextEditingController();
   TextEditingController get search => _search;
 
-  List<Category> _category;
-  List<Category> get category=>_category;
+  ForumCategory _category;
+  ForumCategory get category=>_category;
+
+  ForumCategory _subCategory;
+  ForumCategory get subCategory=>_subCategory;
 
   bool _isLoadingCategory = false;
   bool get isLoadingCategory=> _isLoadingCategory;
 
   Future<void> getCategory() async{
     _isLoadingCategory=true;
-    _apiProvider.getCategory("ru").then((value) => {
+    _apiProvider.getForumCategory("ru").then((value) => {
       _category=value,
+    }).catchError((error){
+      print("Error: $error");
+    }).whenComplete(() => {
+      _isLoadingCategory=false,
+      notifyListeners()
+    });
+  }
+
+  Future<void> getSubCategory() async{
+    _isLoadingCategory=true;
+    _apiProvider.getForumSubCategory("ru").then((value) => {
+      _subCategory=value,
     }).catchError((error){
       print("Error: $error");
     }).whenComplete(() => {
